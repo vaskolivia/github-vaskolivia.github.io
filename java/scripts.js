99c8dd5abbf70652bb40cfb71ce2070ca95cca30
olahnevaskolivia <livus98@gmail.com>
2021-08-09 14:26:49 +0200

    javascript

---
+++ java/scripts.js
@@ -0,0 +1,8 @@
+let nevem = "Livia";
+let korom = 34;
+console.log('A javascript is betoltott');
+console.log(korom);
+if (korom>=18) {
+    console.log('mar eleg idos hogy igyon egy sort');
+} else{
+  console.log('még nem léphetsz italboltba'); }

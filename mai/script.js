a476336853153bf0337e9ef96a9c6dae80014471
olahnevaskolivia <livus98@gmail.com>
2021-08-10 13:01:54 +0200

    try new script

--- mai/script.js
+++ mai/script.js
@@ -1,8 +1,28 @@
 console.log("Oláhné Vaskó Lívia")
-for(let i=0; i < 21; i++){
+/*for(let i=0; i < 21; i++){
    if (i%2==0)
    console.log(i+" ")
 }
 let add = (a, b) => {
    return a + b;
-   console.log(add(1, 2))}
+   console.log(add(1, 2))}*/
+  // function fuggveny(){console.log("adja meg a kívánt értéket") 
+
+/*const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});*/
+//console.log ("adja meg a számot")
+//let n=prompt;
+   //for (let i=0; i<n; i++){
+
+     // console.log(i+" ")
+  // }
+   /*getGreetingTo('Mark');              // kimenet: semmi
+   console.log(getGreetingTo('Mark')); // kimenet: 'Hello Mark!'*/
+//let array= ['1','2','3','4','5','6','7','8']
+   let printValues = (array) => {
+      for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+      }
+    };
